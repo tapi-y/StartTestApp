7e306e88460333f82bfd846b08b7587b65ee7344
tapi-y <tapi09ucat@gmail.com>
2019-07-15 23:24:58 +0900

    first commit

---
+++ app/src/main/java/com/videostrem/starttestapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.videostrem.starttestapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                bundle.putString("url", "file:///android_asset/index.html");
+                String packageName = "com.videostrem.tutorial";
+                String className = "com.videostrem.tutorial.FullscreenActivity";
+                Intent intent = new Intent();
+                intent.setClassName(packageName,className);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+    }
+}
